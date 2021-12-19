65628645e006890735b179677443d087fce9b1dc
ivanyurov <93770384+ivanyurov@users.noreply.github.com>
2021-12-19 16:51:09 +0300

    Create ImgFunctions.h

---
+++ bmp/ImgFunctions.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "BmpStructures.h"
+
+//Utilities
+RgbImg createRgbImg(size_t height, size_t width, RGB color = { 0,0,0 });
+void deleteRgbImg(RgbImg& img);
+RgbImg copyRgbImg(RgbImg const& img);
+RgbImg readRgbImg(char const* filename);
+void writeRgbImg(char const* filename, RgbImg const& img);
+void printImgInfo(char const* filename);
+void rotateImg(char const* input, char const* output, int angle);
+
+//ColorBalancing
+RgbImg autolevels(RgbImg const& img, size_t kern_sz = 0);
+RgbImg grayworld(RgbImg const& img, size_t kern_sz = 0);
+void grayFilter(const char* input, const char* output);
+
+//Filtering
+RgbImg convolution(RgbImg const& img, size_t kernel_size);
+RgbImg convolution(RgbImg const& img, size_t kernel_size, double** kernel);
+RgbImg contours(RgbImg const& img);
