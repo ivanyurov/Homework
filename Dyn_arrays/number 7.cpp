16f1dc5a38c75a81251d53d4bf2001e53d21d9dd
ivanyurov <93770384+ivanyurov@users.noreply.github.com>
2021-12-26 19:37:16 +0300

    Update number 7.cpp

--- Dyn_arrays/number 7.cpp
+++ Dyn_arrays/number 7.cpp
@@ -1,10 +1,11 @@
 void shiftArray(int A[], int n, int k)
 {
     int a;
+    k = k % n;
     for (int i = 0; i < k; ++i)
     {
         a = A[0];
-        for (int j = 0; j < n-1; ++j) A[j] = A[j+1];
+        for (int j = 0; j < n - 1; ++j) A[j] = A[j + 1];
         A[n - 1] = a;
     }
 }
