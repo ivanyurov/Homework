08c1a62ad65c180c4f3152c6c201bd01e842c9e8
ivanyurov <93770384+ivanyurov@users.noreply.github.com>
2021-12-26 21:35:41 +0300

    Update Filtering.cpp

--- bmp/Filtering.cpp
+++ bmp/Filtering.cpp
@@ -90,3 +90,25 @@ RgbImg contours(RgbImg const& img)
     autolevels(result, kern_sz);
     return result;
 }
+void sharpness(char const* input)
+{
+    RgbImg image = readRgbImg(input);
+    double** kernel = new double* [3];
+    double ker[3][3] = { {0, -1, 0}, {-1, 5, -1}, {0, -1, 0} };
+    for (int i = 0; i < 3; ++i) kernel[i] = new double [3];
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            kernel[i][j] = ker[i][j];
+        }
+    }
+    RgbImg image_1 = convolution(image, 5);
+    RgbImg image_2 = convolution(image_1, 3, kernel);
+
+    writeRgbImg("result.bmp", image_2);
+
+    deleteRgbImg(image_2);
+    deleteRgbImg(image_1);
+    deleteRgbImg(image);
+}
